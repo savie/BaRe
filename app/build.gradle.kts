e940c25f1ba1a5afd193a4c3a0a3fa2bccb47d1a
savie <ntop_one@yahoo.co.id>
2026-09-17 00:32:31 +0700

    fix: provide org.json implementation to JVM tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,5 @@ dependencies {
     implementation("dev.rikka.shizuku:api:13.1.5")
     implementation("dev.rikka.shizuku:provider:13.1.5")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.json:json:20250517")
 }
