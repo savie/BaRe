d0ff2b1c81b15b1457bbde53d0f7eb7ef08cfd5b
savie <ntop_one@yahoo.co.id>
2026-09-16 17:00:16 +0700

    refactor: remove obsolete com.savie.bare capability package

--- core/capability/src/main/kotlin/com/savie/bare/core/capability/CapabilityProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.savie.bare.core.capability
-
-import com.savie.bare.core.domain.PrivilegeMode
-
-interface CapabilityProvider {
-    val mode: PrivilegeMode
-    fun detect(): Boolean
-    fun isAuthorized(): Boolean
-}
