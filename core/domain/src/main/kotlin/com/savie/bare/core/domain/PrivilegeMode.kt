ac5bb130b7fb5282b6ee5b298a6242e1b59a333e
savie <ntop_one@yahoo.co.id>
2026-09-16 17:00:09 +0700

    refactor: remove obsolete com.savie.bare domain package

--- core/domain/src/main/kotlin/com/savie/bare/core/domain/PrivilegeMode.kt
+++
@@ -1,9 +0,0 @@
-package com.savie.bare.core.domain
-
-enum class PrivilegeMode { NON_ROOT, ADB, SHIZUKU, ROOT }
-
-data class CapabilityStatus(
-    val mode: PrivilegeMode,
-    val available: Boolean,
-    val authorized: Boolean,
-)
