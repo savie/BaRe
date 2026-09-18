77b4a49154298c696ec6fc61c2c1c58ef0f00122
savie <ntop_one@yahoo.co.id>
2026-09-18 20:02:01 +0700

    refactor: rename source package to com.bare

---
+++ app/src/main/java/com/bare/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.bare
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.bare.app.BaReApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { BaReApp() }
+    }
+}
