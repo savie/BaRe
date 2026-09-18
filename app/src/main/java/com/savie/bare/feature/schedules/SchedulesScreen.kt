dd8cb7c8373a0e92557d319391ef549501ee4517
savie <ntop_one@yahoo.co.id>
2026-09-18 20:02:58 +0700

    refactor: rename source package to com.bare

--- app/src/main/java/com/savie/bare/feature/schedules/SchedulesScreen.kt
+++ app/src/main/java/com/savie/bare/feature/schedules/SchedulesScreen.kt
@@ -1,4 +1,4 @@
-package com.savie.bare.feature.schedules
+package com.bare.feature.schedules
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,9 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.savie.bare.app.Screen
-import com.savie.bare.R
-import com.savie.bare.ui.components.ListEntry
+import com.bare.app.Screen
+import com.bare.R
+import com.bare.ui.components.ListEntry
 
 @Composable
 fun SchedulesScreen(onOpen: (Screen) -> Unit) {
