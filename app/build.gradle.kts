70dab99282eaaa9f6a473a9068c6cb8db0680bf4
savie <ntop_one@yahoo.co.id>
2026-09-18 19:55:39 +0700

    fix: use com.bare application id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.savie.bare"
+        applicationId = "com.bare"
         minSdk = 26
         targetSdk = 35
         versionCode = System.getenv("BARE_VERSION_CODE")?.toIntOrNull() ?: 1
