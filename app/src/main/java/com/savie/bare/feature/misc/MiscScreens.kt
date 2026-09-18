312a4c7f009f4111c029e4f53e4a24ae1d901ae9
savie <ntop_one@yahoo.co.id>
2026-09-18 08:54:57 +0700

    fix: retain Material3 experimental API opt-ins after refactor

--- app/src/main/java/com/savie/bare/feature/misc/MiscScreens.kt
+++ app/src/main/java/com/savie/bare/feature/misc/MiscScreens.kt
@@ -1,5 +1,7 @@
 package com.savie.bare.feature.misc
 
+@file:OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
