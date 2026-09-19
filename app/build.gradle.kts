9ea04118abd82eeeb9ce851210faf7fd763e7a29
savie <ntop_one@yahoo.co.id>
2026-09-19 20:29:53 +0700

    Add recovery unit-test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    testImplementation("junit:junit:4.13.2")
 }
