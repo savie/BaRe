9bce61327db8d3a76b0ae9fc75c630146680dd7d
savie <ntop_one@yahoo.co.id>
2026-09-22 07:34:07 +0700

    fix(settings): opt in to material strategy screen

--- app/src/main/java/com/bare/feature/settings/EncryptionPasswordStrategyScreen.kt
+++ app/src/main/java/com/bare/feature/settings/EncryptionPasswordStrategyScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -28,6 +29,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.bare.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EncryptionPasswordStrategyScreen(
     onBack: () -> Unit,
