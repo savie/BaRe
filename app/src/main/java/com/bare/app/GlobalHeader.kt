6c10c7f7a4e17960b02a86a30ee23700c9ed748e
savie <ntop_one@yahoo.co.id>
2026-09-25 11:42:46 +0700

    fix(ui): use fixed global header geometry

--- app/src/main/java/com/bare/app/GlobalHeader.kt
+++ app/src/main/java/com/bare/app/GlobalHeader.kt
@@ -1,16 +1,15 @@
 package com.bare.app
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
