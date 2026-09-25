74ed47feeacd521fc308b100138049ad876bc5e2
savie <ntop_one@yahoo.co.id>
2026-09-25 08:49:30 +0700

    fix(apps): add label border import

--- app/src/main/java/com/bare/feature/apps/AppsScreens.kt
+++ app/src/main/java/com/bare/feature/apps/AppsScreens.kt
@@ -9,6 +9,7 @@ import android.os.PowerManager
 import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
