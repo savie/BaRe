96ec4ef1555909a8ae377e80264dab8e503d3ad1
savie <ntop_one@yahoo.co.id>
2026-09-26 15:04:08 +0700

    build(a18): add reference tar zstd archive dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,5 +59,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("org.apache.commons:commons-compress:1.27.1")
+    implementation("com.github.luben:zstd-jni:1.5.7-4")
     testImplementation("junit:junit:4.13.2")
 }
